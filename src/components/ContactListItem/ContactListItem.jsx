a496cf686b9f373a4f4652764e85de715f887333
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2022-12-27 15:58:11 +0200

    Create ContactListItem.jsx

---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,19 @@
+// import {
+//   ListItem,
+//   Button,
+// } from 'components/ContactListItem/ContactListItem.styled';
+
+// export const ContactListItem = ({ contacts, removeContact }) => {
+//   return (
+//     <>
+//       {contacts.map(({ id, name, number }) => (
+//         <ListItem key={id}>
+//           {name}: {number}
+//           <Button type="submit" onClick={() => removeContact(id)}>
+//             Delete
+//           </Button>
+//         </ListItem>
+//       ))}
+//     </>
+//   );
+// };
