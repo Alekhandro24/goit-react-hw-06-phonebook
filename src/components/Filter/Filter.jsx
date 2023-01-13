3fdf1b7be7fc867b0a1f224e48fc8708c6a4e905
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-01-13 21:00:07 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,36 @@
-import propTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import { setNameFilter } from 'redux/filtersSlice';
 import { FilterName } from 'components/Filter/Filter.styled';
+import PropTypes from 'prop-types';
 
-export const Filter = ({ filter, handleChange }) => (
-  <div>
-    <label>Find contacts by Name </label>
-    <FilterName
-      type="text"
-      name="filter"
-      placeholder="Enter filter"
-      value={filter}
-      onChange={handleChange}
-    />
-  </div>
-);
+const Filter = () => {
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    dispatch(setNameFilter(e.target.value));
+  };
+
+  if (contacts.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <label>Find contacts by Name </label>
+      <FilterName
+        type="text"
+        name="filter"
+        placeholder="Enter filter"
+        onChange={handleChange}
+      />
+    </>
+  );
+};
+
+export default Filter;
 
 Filter.propTypes = {
-  filter: propTypes.string.isRequired,
-  handleChange: propTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
 };
