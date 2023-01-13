b7d0971861f1b648edd8b4193b9eae1c8f50e56f
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-01-13 20:58:40 +0200

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,19 +1,27 @@
-// import {
-//   ListItem,
-//   Button,
-// } from 'components/ContactListItem/ContactListItem.styled';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContacts } from 'redux/contactsSlice';
+import { Button } from 'components/ContactListItem/ContactListItem.styled';
 
-// export const ContactListItem = ({ contacts, removeContact }) => {
-//   return (
-//     <>
-//       {contacts.map(({ id, name, number }) => (
-//         <ListItem key={id}>
-//           {name}: {number}
-//           <Button type="submit" onClick={() => removeContact(id)}>
-//             Delete
-//           </Button>
-//         </ListItem>
-//       ))}
-//     </>
-//   );
-// };
+const ContactListItem = ({ contact: { id, name, number } }) => {
+  const dispatch = useDispatch();
+  const handleContactDelete = () => dispatch(deleteContacts(id));
+  return (
+    <>
+      {name}: {number}
+      <Button type="button" onClick={handleContactDelete}>
+        Delete
+      </Button>
+    </>
+  );
+};
+
+ContactListItem.propTypes = {
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ContactListItem;
