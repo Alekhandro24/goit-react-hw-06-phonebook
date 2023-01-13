f9035f380900f766e077da725438d07c4fa50a12
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-01-13 20:59:21 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,95 +1,20 @@
-import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
-import { ContactForm } from 'components/ContactForm/ContactForm';
-import { Filter } from 'components/Filter/Filter';
-import { ContactList } from 'components/ContactList/ContactList';
-import { useState, useEffect } from 'react';
+import { Layout } from './Layout';
+import { GlobalStyle } from './GlobalStyle';
+import ContactForm from 'components/ContactForm/ContactForm';
+import Filter from 'components/Filter/Filter';
+import ContactList from 'components/ContactList/ContactList';
 
-export const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-  const [firstRender, setFirstRender] = useState(true);
-
-  useEffect(() => {
-    if (firstRender) {
-      const contactsFromLocalStorage =
-        window.localStorage.getItem('contactList');
-
-      if (contactsFromLocalStorage !== 'undefined') {
-        const parsedContacts = JSON.parse(contactsFromLocalStorage);
-
-        if (parsedContacts) {
-          setContacts(parsedContacts);
-        }
-      }
-      setFirstRender(false);
-    } else {
-      window.localStorage.setItem('contactList', JSON.stringify(contacts));
-    }
-  }, [contacts, firstRender]);
-
-  const handleChange = e => {
-    const { value } = e.target;
-    setFilter(value);
-  };
-
-  const handleSubmit = e => {
-    const id = nanoid();
-    const name = e.name;
-    const number = e.number;
-    const contactsLists = [...contacts];
-
-    if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
-      alert(`${name} is already in contacts.`);
-    } else {
-      contactsLists.push({ id, name, number });
-    }
-
-    setContacts(contactsLists);
-  };
-
-  const handleDelete = e => {
-    setContacts(contacts.filter(contact => contact.id !== e));
-  };
-
-  const getFilteredContacts = () => {
-    const filterContactsList = contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
-    });
-    return filterContactsList;
-  };
-
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 20,
-        color: '#010101',
-      }}
-    >
+    <Layout>
       <h1>Phonebook</h1>
-      <ContactForm handleSubmit={handleSubmit} />
+      <ContactForm />
       <h2> Contacts</h2>
-      <Filter filter={filter} handleChange={handleChange} />
-      <ContactList
-        contacts={getFilteredContacts()}
-        handleDelete={handleDelete}
-      />
-    </div>
+      <Filter />
+      <ContactList />
+      <GlobalStyle />
+    </Layout>
   );
 };
 
-App.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  filter: PropTypes.string,
-};
+export default App;
